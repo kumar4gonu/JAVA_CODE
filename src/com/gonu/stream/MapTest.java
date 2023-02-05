3119f483723b72282baa4195b6896c251407f60d
kumar4gonu <47925670+kumar4gonu@users.noreply.github.com>
2023-02-05 07:58:40 +0530

    new changes

--- src/com/gonu/stream/MapTest.java
+++ src/com/gonu/stream/MapTest.java
@@ -1,4 +1,43 @@
 package com.gonu.stream;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class MapTest {
+    public static void main(String[] args) {
+
+        Student student = new Student("kumar", 30,1);
+        List<Student> list = new ArrayList<>();
+        list.add(student);
+
+        List<String> listOfStudentName = list.stream().map(stu -> stu.getName())
+                .collect(Collectors.toList());
+
+        List<List<Integer> > number = new ArrayList<>();
+
+        // adding the elements to number arraylist
+        number.add(Arrays.asList(1, 2));
+        number.add(Arrays.asList(3, 4));
+        number.add(Arrays.asList(5, 6));
+        number.add(Arrays.asList(7, 8));
+
+
+        System.out.println("List of list-" + number);
+
+        // using flatmap() to flatten this list
+        List<Integer> flatList
+                = number.stream()
+                .flatMap(lists -> lists.stream())
+                .collect(Collectors.toList());
+
+        // printing the list
+        System.out.println("List generate by flatMap-"
+                + flatList);
+
+        listOfStudentName.forEach(System.out::println);
+    }
 }
